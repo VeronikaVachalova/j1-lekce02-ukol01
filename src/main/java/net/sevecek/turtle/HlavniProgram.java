6b8a0b0d7d29e6bf1a4c846a4cddc881f0e42da7
VeronikaVachalova <veronika.vachalova@email.cz>
2022-04-20 21:26:13 +0200

    Nakresleno slunicko.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -1,13 +1,43 @@
 package net.sevecek.turtle;
-
 import net.sevecek.turtle.engine.*;
-
-public class HlavniProgram {
-
+public class HlavniProgram<zofka> {
     public void main(String[] args) {
         Turtle zofka;
-
         zofka = new Turtle();
+        zofka.setLocation(100,80);
+        nakresliSlunicko(zofka);
+        nakresliCaru(zofka);
+        nakresliCaru(zofka);
+        nakresliCaru(zofka);
+        nakresliCaru(zofka);
+        nakresliCaru(zofka);
+        nakresliCaru(zofka);
+        nakresliCaru(zofka);
     }
+        private void nakresliCaru(Turtle zofka) {
+                zofka.turnLeft(90);
+                zofka.move(40);
+                zofka.turnRight(180);
+                zofka.move(40);
+                zofka.turnRight(90);
+                zofka.penUp();
+
+            for (int i = 0; i < 50; i++) {
+                zofka.turnLeft(1);
+                zofka.move(1);
+            }
+            zofka.turnLeft(180);
+            zofka.penDown();
+
+            }
+            private void nakresliSlunicko(Turtle zofka) {
+                for (int i = 0; i < 355; i++) {
+                    zofka.turnRight(1);
+                    zofka.move(1);
+
+
+            }
+            }
+
 
 }
